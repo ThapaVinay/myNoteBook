fb9ad6d7e524a4bc4efffbb3eb8de2b53d00b783
ThapaVinay <thechampthapa@gmail.com>
2023-06-21 14:57:18 +0530

    hashing of the password and auth token added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+// it is secret key to sign the token
+const JWT_Secret = "vinay thapa";
 
 // Create a user using POST "/api/auth/createuser". No login required 
 router.post('/createuser', [
@@ -23,20 +28,31 @@ router.post('/createuser', [
                 return res.status(400).json({ error: "Sorry a user with this email already exists." })
             }
 
+            // salt for the hash
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
             // Create a new user
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPass,
                 email: req.body.email
-            })
+            });
 
-            res.json(user)
+            // using object id as the data for the auth token
+            const data = {
+                user:{
+                    id : user.id
+                }
+            }
+            
+            const authToken = jwt.sign(data,JWT_Secret);
+            res.json({authToken});
         }
         catch (error) {
             console.error(error.message);
             res.status(500).send("Some error occured!");
         }
-
     })
 
 
