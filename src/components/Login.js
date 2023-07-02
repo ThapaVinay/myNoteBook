194d27a04ad33330b1f80fd04ed72abb97d7c2c5
ThapaVinay <thechampthapa@gmail.com>
2023-07-02 12:29:28 +0530

    login page done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import "../styles/Home.css"
+import { TextField, Button, InputAdornment, IconButton } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Login = (props) => {
 
     const [credentials, setCredentials] = useState({ email: "", password: "" });
     const host = "http://localhost:3001";
     let navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,14 +28,14 @@ const Login = (props) => {
             body: JSON.stringify({ email: credentials.email, password: credentials.password })
         });
         const json = await response.json();
-        console.log(json);
 
-        if(json.success) {
+        if (json.success) {
             // save the auth token and redirect
             localStorage.setItem('token', json.authToken);
-            props.showAlert("Logged In Successfully", "success") 
+            props.showAlert("Logged In Successfully", "success")
             navigate('/');   // used to redirect  
-        } else {
+        }
+        else {
             props.showAlert("Invalid Credentials", "danger")
         }
     }
@@ -36,22 +44,51 @@ const Login = (props) => {
         setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
 
+    const handleShowPassword = () => {
+
+        setShowPassword(!showPassword);
+    }
+
+
     return (
-        <div className='container mt-2'>
-            <h2 className='my-3'>Login to continue to iNoteBook</h2>
-            <form onSubmit={handleSubmit} >
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" onChange={onChange} value={credentials.email} />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+
+        <div>
+            <div className="container mt-5 addnotes">
+                <Button className="mb-4" variant="text" color="secondary" startIcon={<ArrowBackIcon />} component={Link} to="/" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif" }}>Home</Button>
+                <h2 style={{ fontWeight: "bold" }}>Login</h2>
+
+                <div className="d-flex justify-content-center my-5">
+                    <Button size="sal" className="mb-4 me-4" variant="contained" color="primary" startIcon={<FacebookIcon />} component={Link} to="/" style={{ textTransform: "none", fontSize: "1.1rem", color: "White", fontFamily: "'Poppins', sans-serif" }}>Login with Facebook</Button>
+                    <Button size="large" className="mb-4" variant="contained" color="error" startIcon={<GoogleIcon />} component={Link} to="/" style={{ textTransform: "none", fontSize: "1.1rem", color: "White", fontFamily: "'Poppins', sans-serif" }}>Login with Google</Button>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" onChange={onChange} value={credentials.password} />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+
+                <p className="mb-4 d-flex justify-content-center"> or Login with Email and Password</p>
+
+                <form noValidate onSubmit={handleSubmit} >
+                    <div className="mb-4">
+                        <TextField variant="outlined" color="secondary" type="email" label="Email" id="email" name="email" onChange={onChange} value={credentials.email} fullWidth />
+                    </div>
+
+                    <div className="mb-4">
+                        <TextField  type={showPassword ? 'text' : 'password'} variant="outlined" color="secondary"  label="Password" id="password" name="password" onChange={onChange} value={credentials.password} fullWidth
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end" key="password-toggle">
+                                        <IconButton onClick={handleShowPassword} aria-label="toggle password visibility" edge="end" >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </div>
+
+                    <Button type="submit" fullWidth size="large" className="mb-4" variant="contained" color="secondary" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "1.1rem" }} >Login</Button>
+                </form>
+                <p> Don't have an account? <Link to="/signup">signup</Link> </p>
+            </div>
         </div>
+
     )
 }
 
