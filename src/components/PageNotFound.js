96e77174db2c0189c1ca77128a95bfb4f9aa7be3
ThapaVinay <thechampthapa@gmail.com>
2023-07-03 09:09:11 +0530

    page not found error corrected

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -10,7 +10,7 @@ function PageNotFound() {
             <h2 className="text-center" style={{fontWeight: "bolder", fontSize: "3rem"}}>404: The Page you are Looking for isn't here</h2>
             <p className="text-center" style={{ fontSize: "0.9rem"}}>You either tried some shady route or you came here by mistake. Whichever it is, try using the navigation</p>
             <img className="img-fluid mt-2" src={PageNotFounds} alt="page not found" style={{width: "50%"}} />
-            <Button className="mb-4" variant="contained" color="secondary" startIcon={<ArrowBackIcon />} component={Link} to="/" style={{ textTransform: "none", color: "White"}}>Go back to home</Button>
+            <Button className="mb-4" variant="contained" color="secondary" startIcon={<ArrowBackIcon />} component={Link} to="/myNoteBook" style={{ textTransform: "none", color: "White"}}>Go back to home</Button>
         </div>
     )
 }
